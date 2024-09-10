1c0ebc014c217d43e5dbb2292cf09917088dcefe
Ashish Gupta <ashish.gupta@sign3labs.com>
2024-09-10 19:11:17 +0530

    add Objective c file and doint setup

---
+++ Sign3Intelligence/Sign3Intelligence/SimulatorChecker.m
@@ -0,0 +1,45 @@
+//
+//  SimulatorChecker.m:  SimulatorChecker.m:  SimulatorChecker.m
+//  Sign3Intelligence
+//
+//  Created by Ashish Gupta on 10/09/24.
+//
+
+#import <Foundation/Foundation.h>
+#import "SimulatorChecker.h"
+#import <UIKit/UIKit.h>
+#include <mach-o/dyld.h>
+
+@implementation SimulatorChecker
+
+- (BOOL)isSimulator {
+    int simulator = 0;
+#if TARGET_IPHONE_SIMULATOR
+    simulator = 1;
+#else
+    simulator = 0;
+#endif
+    if (!simulator) {
+        uint64_t dyld_simulator = 0;
+        uint32_t dyld_count = _dyld_image_count();
+        NSString* dyld_sim_str = @"usr/lib/dyld_sim";
+        NSString* CoreSimulator_str = @"Library/Developer/CoreSimulator";
+        char* cEmptyString = "";
+        for (uint32_t i = 0; i < dyld_count; i++) {
+            @autoreleasepool {
+                if (i > 10000) break;
+                NSString *dyld_image_name = [NSString stringWithCString:_dyld_get_image_name(i) ?: cEmptyString encoding:NSUTF8StringEncoding];
+                if ([dyld_image_name hasSuffix:dyld_sim_str]) {
+                    dyld_simulator |= 1;
+                }
+                if ([dyld_image_name containsString:CoreSimulator_str]) {
+                    dyld_simulator |= 1 << 1;
+                }
+            }
+        }
+        simulator = (dyld_simulator ? 1 : 0);
+    }
+    return simulator;
+}
+
+@end
