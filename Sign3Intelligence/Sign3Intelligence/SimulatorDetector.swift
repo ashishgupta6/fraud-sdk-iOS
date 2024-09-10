8d7578ab2178946308d3a4ec4f4c1c440a720c09
Ashish Gupta <ashish.gupta@sign3labs.com>
2024-09-10 19:11:30 +0530

    add Objective c file and doint setup

--- Sign3Intelligence/Sign3Intelligence/SimulatorDetector.swift
+++ Sign3Intelligence/Sign3Intelligence/SimulatorDetector.swift
@@ -1,14 +1,15 @@
 //
-//  SimulatorDetector.swift
-//  Sign3Intelligence
+// SimulatorDetector.swift
+// Sign3Intelligence
 //
-//  Created by Ashish Gupta on 09/09/24.
+// Created by Ashish Gupta on 09/09/24.
 //
-
 import Foundation
+//#import; "Sign3Intelligence/SimulatorChecker.h"
 
 
 class SimulatorDetector{
+    
     let TAG = "SimulatorDetector"
     
     func isSimulatorDetected() async -> Bool{
@@ -17,12 +18,35 @@ class SimulatorDetector{
             requestId: UUID().uuidString,
             defaultValue: false,
             function: {
-                #if targetEnvironment(simulator)
-                    return true
-                #else
-                    return false
-                #endif
+                var simulatorFlag = false
+                if (!simulatorFlag){
+                    simulatorFlag = checkSimulator()
+                }
+                if (!simulatorFlag){
+                    simulatorFlag = checkSimulatorFromObjectiveC()
+                }
+                return simulatorFlag
             }
         )
+    }
+    
+    
+    func checkSimulator() -> Bool{
+#if targetEnvironment(simulator)
+        return true
+#else
+        return false
+#endif
+    }
+    
+    
+    func checkSimulatorFromObjectiveC() -> Bool{
+        let simulatorChecker = SimulatorChecker()
+//        if simulatorChecker.isSimulator() {
+//            return true
+//        } else {
+//            return false
+//        }
+        return false
     }
 }
